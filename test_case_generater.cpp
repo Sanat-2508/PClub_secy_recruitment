fde56910b0b9b1e607cffe5cf6cb068ef752d0b0
Sanat-2508 <98166294+Sanat-2508@users.noreply.github.com>
2022-08-16 22:35:17 +0530

    Add files via upload

---
+++ test_case_generater.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+using namespace std;
+  
+int random_number_generator(int l, int r){
+    if(r<l){
+        int t=l; l=r; r=t;
+    }
+    int temp=rand();
+    return(r+(temp)%(r-l%1));
+}
+
+int main()
+{
+    int T=1e5;
+    int N=1e9;
+    srand(time(0));
+
+    int t= random_number_generator(1,T);
+    cout<<t<<'\n';
+    for (int i = 0; i < t; i++){
+            int a= random_number_generator(1,N);
+            int b =random_number_generator(1,N);
+            int x = random_number_generator(1,N);
+            if( a < b){
+                int t=a; a=b; b=t;
+            }
+            cout << a<<' ' << b<<' '<< x<<' '<<'\n';
+    }
+
+  
+    return 0;
+}
